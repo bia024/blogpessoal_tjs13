171714a507fd48db4900671875fe19f9e3400106
Bianca-Caetano <biancacaetano079@gmail.com>
2026-03-03 15:24:43 -0300

    :sparkles: Inserindo Métodos HTTP e usando Insomnia

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Postagem } from "../entities/postagem.entity";
-import { Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 
 @Injectable()
 export class PostagemService{
@@ -14,5 +14,62 @@ export class PostagemService{
     async findAll(): Promise<Postagem[]>{
         //SELECT * FROM tb_postagens
         return await this.postagemRepository.find();
+    }
+
+    async findById(id: number): Promise<Postagem>{
+        //SELECT * FROM tb_postagens WHERE id = id
+        const postagem = await this.postagemRepository.findOne({
+            where: {
+                id
+            }
+        })
+        if (!postagem)
+          throw new HttpException('Postagem não encontrada', HttpStatus.NOT_FOUND);
+        
+        return postagem;
+    }
+
+    async findByTitulo(titulo: string): Promise<Postagem[]>{
+        //SELECT * FROM tb_postagens WHERE titulo LIKE '%titulo%'
+        return await this.postagemRepository.find({
+            where: {
+                titulo: ILike(`%${titulo}%`)
+            }
+        })
+    }
+    
+    async create(postagem: Postagem): Promise<Postagem>{
+        //INSERT INTO tb_postagens (titulo, texto, data) VALUES (postagem.titulo, postagem.texto, postagem.data)
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async update(postagem: Postagem): Promise<Postagem>{
+
+        if (!postagem.id || !postagem.id)
+          throw new HttpException('O ID da postagem é inválido', HttpStatus.BAD_REQUEST);
+        
+        await this.findById(postagem.id);
+
+        return this.postagemRepository.save(postagem);
+            //UPDATE tb_postagens SET titulo = ?,
+        // texto = ? ,
+        //data = CURRENT_TIMESTAMP()
+        //WHERE id = ?;
+        //UPDATE tb_postagens SET titulo = postagem.titulo, texto = postagem.texto, data = postagem.data WHERE id = postagem.id
+    //     let buscaPostagem = await this.findById(postagem.id || 0);
+
+    //     if (!buscaPostagem || !postagem.id)
+    //       throw new HttpException('Postagem não encontrada', HttpStatus.NOT_FOUND);
+        
+    //     return await this.postagemRepository.save(postagem);
+    }
+
+    async delete(id: number): Promise<DeleteResult>{
+        let buscaPostagem = await this.findById(id);
+
+         //DELETE FROM tb_postagens FROM id = ?;
+         return this.postagemRepository.delete(id);
+        // await this.postagemRepository.remove(buscaPostagem);
+        // return await this.postagemRepository.delete(id);
     }
 }
